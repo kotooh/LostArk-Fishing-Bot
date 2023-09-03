7924503d3bb1838ae69b0a332fc727dd5c1af4a3
Kevin Tu <kevintu002@gmail.com>
2023-09-03 12:48:56 -0400

    pet auto repair

--- main.py
+++ main.py
@@ -3,38 +3,78 @@ import random
 import cv2
 import keyboard
 import numpy as np
+import pydirectinput
 
 from PIL import ImageGrab
-
-cast = True
-fishing = cv2.imread('image.png', 0)
-templateWidth, templateHeight = fishing.shape[::-1]
 
 print("Fishing will be started in 5 seconds!")
 time.sleep(5)
 print("Started!")
 
-while True:
-    if (cast):
-        print("Casting out lure")
-        keyboard.press_and_release('e')
-        cast = False
-        continue
+def main():
+    cast = True
+    fishing = cv2.imread('image.png', 0)
+    templateWidth, templateHeight = fishing.shape[::-1]
+    successCount = 1
 
-    imagePixels = ImageGrab.grab(bbox=(0, 0, 1920, 1080))
-    pixelsNumpy = np.array(imagePixels)
+    while True:
+        # repair
+        if successCount % 10 == 0:
+            togglePetWindow()
 
-    grayscaleImage = cv2.cvtColor(pixelsNumpy, cv2.COLOR_BGR2GRAY)
-    templateMatch = cv2.matchTemplate(
-        grayscaleImage, fishing, cv2.TM_CCOEFF_NORMED)
-    matchLocations = np.where(templateMatch >= 0.8)
+            pydirectinput.click(x=1245, y=700, button="left")
+            sleep(400, 500)
+            pydirectinput.click(x=730, y=800, button="left")
+            sleep(400, 500)
+            pydirectinput.click(x=915, y=630, button="left")
+            sleep(400, 500)
+            pydirectinput.press("esc")
+            sleep(400, 500)
 
-    for point in zip(*matchLocations[::-1]):
-        if point != None:
-            print("Detected catch! Reeling in lure")
+            togglePetWindow()
+            successCount += 1
+            
+            pydirectinput.click(x=777, y=250, button="right")
+            sleep(400, 500)
+
+        if (cast):
+            print("Casting out lure")
             keyboard.press_and_release('e')
-            cast = True
-            time.sleep(random.uniform(6, 7.5))
-            break
+            cast = False
+            continue
 
-    time.sleep(0.100)
+        imagePixels = ImageGrab.grab(bbox=(0, 0, 1920, 1080))
+        pixelsNumpy = np.array(imagePixels)
+
+        grayscaleImage = cv2.cvtColor(pixelsNumpy, cv2.COLOR_BGR2GRAY)
+        templateMatch = cv2.matchTemplate(
+            grayscaleImage, fishing, cv2.TM_CCOEFF_NORMED)
+        matchLocations = np.where(templateMatch >= 0.8)
+
+        for point in zip(*matchLocations[::-1]):
+            if point != None:
+                print("Detected catch! Reeling in lure")
+                keyboard.press_and_release('e')
+                cast = True
+                time.sleep(random.uniform(6, 7.5))
+                break
+
+        time.sleep(0.100)
+
+def sleep(min, max):
+    sleepTime = random.randint(min, max) / 1000.0
+    if sleepTime < 0:
+        return
+    time.sleep(sleepTime)
+
+def togglePetWindow():
+    pydirectinput.keyDown('alt')
+    sleep(10, 30)
+    pydirectinput.press('p')
+    sleep(10, 30)
+    pydirectinput.keyUp('alt')
+    sleep(10, 30)
+
+    sleep(400, 500)
+
+main()
