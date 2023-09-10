0f514a26e2095350fa57badc0441c19940993a42
Kevin Tu <kevintu002@gmail.com>
2023-09-10 12:58:53 -0400

    recast

--- main.py
+++ main.py
@@ -4,44 +4,54 @@ import cv2
 import keyboard
 import numpy as np
 import pydirectinput
+import datetime
 
 from PIL import ImageGrab
 
+fishX, fishY = 1111, 263
+
+fishing = cv2.imread('image.png', 0)
+templateWidth, templateHeight = fishing.shape[::-1]
+
 print("Fishing will be started in 5 seconds!")
+pydirectinput.click(x=fishX, y=fishY, button="right")
 time.sleep(5)
 print("Started!")
 
 def main():
     cast = True
-    fishing = cv2.imread('image.png', 0)
-    templateWidth, templateHeight = fishing.shape[::-1]
     successCount = 1
+    lastCastTimestamp = None
 
     while True:
+        currentTime = datetime.datetime.now()
+
         # repair
         if successCount % 10 == 0:
             sleep(5000, 5000)
             togglePetWindow()
 
             pydirectinput.click(x=1245, y=700, button="left")
-            sleep(400, 500)
+            sleep(1000, 1000)
             pydirectinput.click(x=730, y=800, button="left")
-            sleep(400, 500)
+            sleep(1000, 1000)
             pydirectinput.click(x=915, y=630, button="left")
-            sleep(400, 500)
+            sleep(1000, 1000)
             pydirectinput.press("esc")
-            sleep(400, 500)
+            sleep(1000, 1000)
 
             togglePetWindow()
             successCount += 1
             
-            pydirectinput.click(x=1200, y=250, button="right")
+            pydirectinput.click(x=fishX, y=fishY, button="right")
             sleep(2000, 2000)
 
-        if (cast):
+        if cast or currentTime >= lastCastTimestamp + datetime.timedelta(seconds = 30):
             print("Casting out lure")
             keyboard.press_and_release('e')
             cast = False
+            
+            lastCastTimestamp = currentTime
             continue
 
         imagePixels = ImageGrab.grab(bbox=(0, 0, 1920, 1080))
@@ -62,7 +72,7 @@ def main():
                 successCount += 1
                 break
 
-        time.sleep(0.100)
+        time.sleep(0.500)
 
 def sleep(min, max):
     sleepTime = random.randint(min, max) / 1000.0
